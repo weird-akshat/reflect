139ecc40f833bd494f2aae840c95eb121d7a7772
Akshat Pandey <thatweirdakshat@gmail.com>
2025-10-03 16:44:27 +0530

    fix token expired token bug

--- src/main/java/com/lifemanagement/reflect/security/JwtHelper.java
+++ src/main/java/com/lifemanagement/reflect/security/JwtHelper.java
@@ -63,9 +63,12 @@ public class JwtHelper {
     //   compaction of the JWT to a URL-safe string
     private String doGenerateToken(Map<String, Object> claims, String subject) {
 
-        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + tokenValidity * 1000))
+        String x= Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + tokenValidity ))
                 .signWith(SignatureAlgorithm.HS512, secret).compact();
+        System.out.println(x);
+        System.out.println(System.currentTimeMillis()+tokenValidity*1000);
+        return x;
     }
 
     //validate token
